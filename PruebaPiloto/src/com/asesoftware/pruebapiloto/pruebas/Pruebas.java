76727712a84163798da9e6ab765a1f142d629c9c
CARLOS JAVIER PITA AMAYA <javierpita17@hotmail.com>
2018-08-09 10:48:28 -0500

    Añadir pruebas con JUnit

---
+++ PruebaPiloto/src/com/asesoftware/pruebapiloto/pruebas/Pruebas.java
@@ -0,0 +1,14 @@
+package com.asesoftware.pruebapiloto.pruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Pruebas {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
