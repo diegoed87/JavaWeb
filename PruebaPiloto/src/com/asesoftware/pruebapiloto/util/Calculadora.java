6c35c5415082f3b155b5db3bdb18976e09ce2d5a
CARLOS JAVIER PITA AMAYA <javierpita17@hotmail.com>
2018-08-11 14:28:40 -0500

    Se realiza cambios al proyecto, página de inicio en web.xml

--- PruebaPiloto/src/com/asesoftware/pruebapiloto/util/Calculadora.java
+++ PruebaPiloto/src/com/asesoftware/pruebapiloto/util/Calculadora.java
@@ -1,54 +1,61 @@
 package com.asesoftware.pruebapiloto.util;
 
 public class Calculadora {
-
-private int memoria;
-	
-	public Calculadora() {
-		memoria = 0;
+	//Variables de instancia
+	private int memoria;
+	// Constructor
+	public Calculadora(){
+		memoria=0;
 	}
-	
-	public int sumar(int a, int b) {
-		memoria =  a+b;
-		return memoria;
-	} 
-	
-	public int restar(int a, int b) {
-		memoria = a - b;
+	//Metodo sumar
+	public int sumar(int a, int b){
+		memoria= a+b;
 		return memoria;
 	}
-	
-	public int dividir(int a, int b) {
-		if (b==0) {
+	//Metodo restar
+	public int restar(int a, int b){
+		memoria= a-b;
+		return memoria;
+	}
+	//Metodo add
+	public int add(int vlr){
+		memoria+=vlr;
+		return memoria;
+	}
+	//Metodo sub
+	public int sub(int vlr){
+		memoria-=vlr;
+		return memoria;
+	}
+	//Getter
+	public int getMemoria(){
+		return memoria;
+	}
+	//Metodo clear
+	public void clear(){
+		memoria=0;
+	}
+	//Metodo multiplicar
+	public int multiplicar(int a, int b){
+		memoria= a*b;
+		return memoria;
+	}
+	//Metodo divididr
+	public int dividir(int a, int b){
+		if(b==0){
 			throw new ArithmeticException("No es posible dividir entre cero");
 		}
-		memoria = a/b;
+		memoria= a/b;
 		return memoria;
-	}
-	
-	public int add(int valor) {
-		memoria += valor;
-		return memoria;
-	}
-	
-	public int sub(int valor) {
-		memoria -= valor;
-		return memoria;
-	}
-	
-	public int memoria() {
-		return memoria;
-	}
-	
-	public void clear() {
-		memoria = 0;
 	}
 	
 	public void metodoOptimo() {
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(500);
 		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
 }
