d3237098fd91331a25fe32566c9728b56d6b3af7
DESKTOP-E4D3695\USUARIO <javierpita17@gmail.com>
2018-09-03 17:02:50 -0500

    Se agregan casos de prueba para análisis Sona Qube

---
+++ PruebaPiloto/src/main/java/com/asesoftware/pruebapiloto/entidades/PruebaSonar.java
@@ -0,0 +1,27 @@
+package com.asesoftware.pruebapiloto.entidades;
+
+import java.util.Date;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+public class PruebaSonar {
+	
+	
+	public int  metodo() {
+		int a = 9;
+		 a =+ 9;
+		 return a;
+	}
+	
+	public void metodo2() {
+	}
+	
+	public void metodo3() {
+		Date d = new Date();
+		int anio = d.getYear();
+		int mes = d.getMonth();
+		int dia = d.getDay();
+	}
+
+}
